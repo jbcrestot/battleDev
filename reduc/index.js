915bd7b16abbb9718751fde1d6c6a215b87f28b6
Jean-Baptiste Crestot <jbcrestot@gmail.com>
2018-03-27 21:00:46 +0200

    2

--- reduc/index.js
+++ reduc/index.js
@@ -1,18 +1,14 @@
-"use strict";
-
 var fileToString = require("../fileToString.js");
 var input = fileToString.readFile("input2.txt");
 
 // response
 
-var prixBuffet = parseInt(input.shift());
-var nbTable = parseInt(input.shift());
-input = input.map(function (x) {
-  return parseInt(x);
-});
+const prixBuffet = parseInt(input.shift());
+const nbTable = parseInt(input.shift());
+input = input.map(x => parseInt(x));
 console.log(input, prixBuffet, nbTable);
 
-var getReduc = function getReduc(nbPers) {
+const getReduc = nbPers => {
   if (nbPers >= 10) {
     return 30;
   } else if (nbPers >= 6) {
@@ -26,8 +22,8 @@ var getReduc = function getReduc(nbPers) {
 
 // console.log("calc", 10 - 10 * 30 / 100);
 
-var getPrixTable = function getPrixTable(table) {
-  var reduc = getReduc(table);
+const getPrixTable = table => {
+  const reduc = getReduc(table);
   console.log(table, reduc, table * prixBuffet);
   console.log(table * prixBuffet - table * prixBuffet * reduc / 100);
   return table * prixBuffet - table * prixBuffet * reduc / 100;
@@ -35,8 +31,8 @@ var getPrixTable = function getPrixTable(table) {
 
 // getPrixTable(input[0]);
 
-var prixTable = 0;
-input.forEach(function (table) {
+let prixTable = 0;
+input.forEach(table => {
   //   console.log(prixTable, getPrixTable(table));
   prixTable += getPrixTable(table);
 });
