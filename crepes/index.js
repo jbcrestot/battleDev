003250487ce384412826cf9d7410676e15b74461
Jean-Baptiste Crestot <jbcrestot@gmail.com>
2018-04-04 09:59:55 +0200

    crepes

--- crepes/index.js
+++ crepes/index.js
@@ -1,7 +1,3 @@
-"use strict";
-
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
 var fileToString = require("../fileToString.js");
 var input = fileToString.readFile("input1.txt");
 
@@ -11,52 +7,45 @@ var input = fileToString.readFile("input1.txt");
 // Résultat attendu : 5
 
 // response
-input = input.map(function (x) {
-  return parseInt(x);
-});
+input = input.map(x => parseInt(x));
 // console.log(input);
-var sortedArray = [].concat(_toConsumableArray(input)).sort(function (a, b) {
-  return a - b;
-});
+const sortedArray = [...input].sort((a, b) => a - b);
 // console.log(sortedArray);
 
-var retourneFromPos = function retourneFromPos(intPile, pos) {
-  var pile = [].concat(_toConsumableArray(intPile));
-  var partPile = pile.splice(0, pos + 1);
+const retourneFromPos = (intPile, pos) => {
+  const pile = [...intPile];
+  const partPile = pile.splice(0, pos + 1);
   // console.log("dois retourner", partPile);
   partPile.reverse();
   // console.log("ce qui donne", partPile);
   // console.log("--------------------------");
 
-  return [].concat(_toConsumableArray(partPile), _toConsumableArray(pile));
+  return [...partPile, ...pile];
 };
 
-var nbMatches = [0, 0, 0, 0, 0, 0, 0, 0];
+let nbMatches = [0, 0, 0, 0, 0, 0, 0, 0];
 
-var isMatching = function isMatching(entry) {
-  var inputToTest = [].concat(_toConsumableArray(entry));
+const isMatching = entry => {
+  const inputToTest = [...entry];
   // console.log(
   //   `${JSON.stringify(inputToTest)} === ${JSON.stringify(sortedArray)}`
   // );
   return JSON.stringify(inputToTest) === JSON.stringify(sortedArray);
 };
 
-var testMatch = function testMatch(nbCoups) {
-  var tempInput = [].concat(_toConsumableArray(input));
+const testMatch = nbCoups => {
+  let tempInput = [...input];
   // console.log("nbCoups", nbCoups);
 
-  var loop = function loop(arrayToTest) {
-    var profondeur = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-    var previousSwitch = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
-
+  const loop = (arrayToTest, profondeur = 1, previousSwitch = null) => {
     // console.log();
     // console.log("--------------- loop prof ", profondeur);
     // console.log("arrayToTest", arrayToTest);
 
-    for (var index = 1; index < input.length; index++) {
+    for (let index = 1; index < input.length; index++) {
       // console.log("previousSwitch", previousSwitch, "index", index);
       if (previousSwitch !== index) {
-        var tempArr = retourneFromPos(arrayToTest, index);
+        const tempArr = retourneFromPos(arrayToTest, index);
         if (isMatching(tempArr)) {
           nbMatches[profondeur]++;
         }
@@ -81,6 +70,4 @@ if (isMatching(input)) {
 }
 testMatch(7);
 // console.log("nbMatchesMin", nbMatches.findIndex(x => x > 0));
-console.log(nbMatches.findIndex(function (x) {
-  return x > 0;
-}));
+console.log(nbMatches.findIndex(x => x > 0));
